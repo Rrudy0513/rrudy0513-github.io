badebd3f46ce86656c07848adc26540a703de41f
rrudy0513 <rrudy0513@gmail.com>
2020-07-23 13:20:24 -0400

    add new code

---
+++ test.js
@@ -0,0 +1,12 @@
+let softwareJobs = [
+  {
+    jobPosting: {
+      title: 'Full Stack Web Developer',
+      location: 'Florida',
+      salary: 50000
+    }
+  }
+]
+
+// Do not edit above this line.
+console.log(`softwareJob`);
